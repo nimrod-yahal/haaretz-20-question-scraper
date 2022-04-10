1a48b6f47c851ef25db154eee1e73c445305ad31
Nimrod Yahal <nimrodyahal@gmail.com>
2022-04-10 19:38:22 +0300

    Improvements

--- scraper.py
+++ scraper.py
@@ -1,9 +1,14 @@
 # coding: utf-8
 import requests
 import json
+import logging
+import os
 from bs4 import BeautifulSoup, SoupStrainer
 from urllib.parse import urljoin
+from os import path
 
+
+PRODUCT_PATH = 'Product'
 
 LINK_SCRAPING_URL = 'https://graphql.haaretz.co.il:443/'
 LINK_SCRAPING_HEADERS = {
@@ -37,15 +42,15 @@ def scrape_test_links(max_page_index):
 		}
 		response = requests.post(LINK_SCRAPING_URL, headers=LINK_SCRAPING_HEADERS, json=query_json)
 
-		links.extend([item['path'] for item in json.loads(response.text)['data']['list']['items']])
-	return links
+		for link in [item['path'] for item in json.loads(response.text)['data']['list']['items']]:
+			yield link
 
 
 def get_test_data(test_link):
 	url = urljoin(BASE_URL, test_link)
 
 	response = requests.get(url, headers=HEADERS)
-	parsed_html = BeautifulSoup(response.text)
+	parsed_html = BeautifulSoup(response.text, features='html.parser')
 
 	questions = parsed_html.body.find_all('div', attrs={'class': QUESTIONS_CLASS})
 	answers = parsed_html.body.find_all('div', attrs={'class': ANSWERS_CLASS})
@@ -53,12 +58,37 @@ def get_test_data(test_link):
 	stripped_questions = [div.text for div in questions]
 	stripped_answers = [div.text for div in answers]
 
-	return list(zip(stripped_questions, stripped_answers))
+	return zip(stripped_questions, stripped_answers)
+
+
+def dump_test_data(test_data, file_handler):
+	for q, a in test_data:
+		file_handler.write(f'Q: {q}\nA: {a}\n\n')
 
 
 def main():
-	tests_data = []
-	for test_url in scrape_test_links(10):
+	logger = logging.getLogger()
+	logger.setLevel(logging.INFO)
+
+	if not path.exists(PRODUCT_PATH):
+		os.makedirs(PRODUCT_PATH)
+
+	for test_url in scrape_test_links(999):
+		test_url_name = test_url.split('/')[-1]
+
+		with open(path.join(PRODUCT_PATH, 'index.txt'), 'a') as f:
+			f.write(f'{test_url_name}\n')
+
+		logging.info(f'Scraping test url: {test_url_name}')
 		test_data = get_test_data(test_url)
-		tests_data.extend(test_data)
-	return tests_data
+		if test_data:
+			with open(path.join(PRODUCT_PATH, f'{test_url_name}.txt'), 'w') as f:
+				dump_test_data(test_data, f)
+		else:
+			logging.error(f'FAILED on test url: {test_url_name}')
+			with open(path.join(PRODUCT_PATH, 'failed-urls.txt'), 'a') as f:
+				f.write(f'{test_url_name}\n')
+
+
+if __name__ == '__main__':
+	main()
